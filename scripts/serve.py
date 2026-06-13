cc9c3fbe612cedc0ce09c25b3b6f8b3a006dadb9
Noman-Ahmad25 <nomanahmad9356@gmail.com>
2026-06-13 12:29:44 +0530

    refactor(api): use API settings in serve script

--- scripts/serve.py
+++ scripts/serve.py
@@ -2,11 +2,11 @@
 """"Start the fu7ur3pr00f API server"""
 
 import uvicorn
-
+from fu7ur3pr00f.api.config import settings
 if __name__ == "__main__":
     uvicorn.run(
         "fu7ur3pr00f.api.app:app",
-        host="0.0.0.0",
-        port=8000,
+        host=settings.host,
+        port=settings.port,
         reload=True,
     )
